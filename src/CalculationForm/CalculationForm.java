19e1de11e14cd322f7207fc1e09f25275bb95eec
Sarah Jessica Leivers <sarahjessica.leivers@bbc.co.uk>
2015-09-22 09:53:43 +0100

    wallpapers

--- src/CalculationForm/CalculationForm.java
+++ src/CalculationForm/CalculationForm.java
@@ -1,8 +1,14 @@
 package CalculationForm;
 
+import Data.Wallpapers;
+
 /**
  * Created by leives01 on 21/09/15.
  */
 public class CalculationForm {
 
+    Wallpapers awningStripe = new Wallpapers(true, "Awning Stripe", 52, 0);
+    Wallpapers countryRoses = new Wallpapers(true, "Country Roses", 51, 15);
+    Wallpapers tropicalBirds = new Wallpapers(false, "Tropical Birds", 52, 60);
+    Wallpapers polkaDot = new Wallpapers(true, "Polka Dot", 53, 3);
 }
--- src/Data/Wallpapers.java
+++ src/Data/Wallpapers.java
@@ -4,12 +4,16 @@ package Data;
  * Created by leives01 on 21/09/15.
  */
 public class Wallpapers {
+
     public boolean inStock;
     public String name;
-    public integer width;
+    public int width;
+    public int patternRepeat;
 
 
-    public Wallpapers(inStock, name, width){
-        
+    public Wallpapers(boolean inStock, String name, int width, int patternRepeat){
+        this.inStock = inStock;
+        this.name=name;
+        this.width=width;
     }
 }
